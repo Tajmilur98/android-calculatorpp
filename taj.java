2bcd5b0b9432ad53eabb03abbd5f51076328301d
tajmilur-rahman <rahman1807100@stud.kuet.ac.bd>
2022-05-08 22:35:31 +0600

    taj.java file added

---
+++ taj.java
@@ -0,0 +1,25 @@
+public class my_class{
+    String name;
+    int roll;
+
+    my_class(String s, int n){
+        name = s;
+        roll = n;
+    }
+
+    public int getRoll(){
+        return roll;
+    }
+    public int getName(){
+        return name;
+    }
+    public void setRoll(int r){
+        roll =r;
+    }
+
+    public void setName(String n){
+        name =n;
+    }
+
+}
+
